7a66c362b3667128394dddb3794ef1c0272d08ab
Ashley Cliff <ashley.cliff@canonical.com>
2025-09-17 17:49:20 -0400

    docs: tutorial ordering and step details
    Signed-off-by: Ashley Cliff <ashley.cliff@canonical.com>

--- reuse/tutorial/submit_hello.sh
+++ reuse/tutorial/submit_hello.sh
@@ -2,8 +2,8 @@
 #SBATCH --job-name=hello_world
 #SBATCH --partition=tutorial-partition
 #SBATCH --nodes=2
-#SBATCH --error=/data/tutorial/error.txt
-#SBATCH --output=/data/tutorial/output.txt
+#SBATCH --error=/data/mpi_example/error.txt
+#SBATCH --output=/data/mpi_example/output.txt
 
-mpirun /data/tutorial/mpi_hello_world
+mpirun /data/mpi_example/mpi_hello_world
 
